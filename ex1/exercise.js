f2504d53a70cc8041b11a7c94c5274d3ea3d05bf
gando diallo <gandoesprit@gmail.com>
2021-03-04 11:42:06 +0100

    Solution to exercice 1

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,13 @@
-// define your functions here
 
+var aElem = document.querySelector('a');
+
+aElem.addEventListener('mouseover', function modifiColor(){
+    aElem.style.color = 'green';
+    aElem.style.fontWeight = 'bold';
+});
+
+aElem.addEventListener('mouseout', function backColor(){
+    aElem.style.color = 'black';
+    aElem.style.fontWeight = 'normal';
+})
 // call the functions and connect them to an event
