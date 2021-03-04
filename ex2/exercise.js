3d77b3253a58601b47a0095e2fcf9044e3c44e88
gando diallo <gandoesprit@gmail.com>
2021-03-04 12:19:43 +0100

    Solution to exercice 2

---
+++ ex2/exercise.js
@@ -0,0 +1,18 @@
+var pElement = document.getElementsByTagName('p')[1];
+
+var bodyElement = document.querySelector('body');
+
+window.addEventListener('resize', function(){
+    var weight = window.innerWidth
+    var height = window.innerHeight;
+
+    pElement.innerHTML = 'The window is <strong>' + weight + ' pixel</strong> width and <strong>' + height + ' pixel height</strong>';
+
+    if(weight === 500){
+        bodyElement.style.backgroundColor = 'green';
+    }else if(weight === 600){
+        bodyElement.style.backgroundColor = 'blue';
+    }else if(weight >= 700){
+        bodyElement.style.backgroundColor = 'yellow';
+    }
+});
