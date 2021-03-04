ce2e7743aee199735879b45844cfabdf3af7f16d
gando diallo <gandoesprit@gmail.com>
2021-03-04 15:01:54 +0100

    Add folder images to exercice 4

---
+++ ex4/exercise.js
@@ -0,0 +1,24 @@
+var inputElement = document.getElementById('name');
+
+var pTag = document.createElement('p')
+
+var obj = {dog: '/images/dog.jpg', cat: '/images/cat.jpg', turtle: '/images/turtle.jpg'}
+
+const keysObj = Object.keys(obj);
+
+var imgTag = document.createElement('img');
+
+var pTagImg = document.createElement('p');
+
+inputElement.addEventListener('input', function(){
+    pTag.innerHTML = inputElement.value;
+    document.body.appendChild(pTag);
+
+    keysObj.forEach(function(element){
+        if(element === inputElement.value){
+            imgTag.setAttribute('src', obj + '.' + element);
+            pTagImg.appendChild(imgTag);
+            document.body.appendChild(pTagImg);
+        }
+    })
+})
